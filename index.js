26bc001973fd9ad3fbaafc96921d73e8cab73e81
sergey.zinovev1994 <sergey.zinovev1994@yandex.ru>
2024-09-28 17:09:47 +0300

    update index.js and update src/index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import _ from "lodash";
+import _ from 'lodash';
 
-import something from "src/indexedDB.js"
+import something from 'src/indexedDB.js';
 
 something();
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import _ from "lodash";
+import _ from 'lodash';
 
 const something = () => {
-console.log('Hello, World');
-console.log(_.last(['one', 'two']));
+  console.log('Hello, World');
+  console.log(_.last(['one', 'two']));
 };
 
 export default something;
-
